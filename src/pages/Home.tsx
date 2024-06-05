a93cb4eedef5c9c5a618185bc99fce5d78479a06
rigolinux <rigoberto7567@gmail.com>
2024-06-05 14:04:05 -0600

    update home view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,31 @@
-
+import  { useContext } from 'react';
+import { PokemonContext } from '../context/PokemonContext';
+import PokemonCard from '../components/PokemonCard';
+import Pagination from '../components/Pagination';
+import { Search } from '../components';
 export const Home = () => {
+  const pokemonContext = useContext(PokemonContext);
+
+  if (!pokemonContext) {
+    return <div>Loading...</div>;
+  }
+
+  const { pokemons } = pokemonContext;
+
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
-  )
-}
+    <>
+      <h1 className="text-4xl text-center font-bold">Poke-Api</h1>
+        <Pagination />
+        <Search />
+      <div className="flex  justify-center mt-5">
+        <div className="container card-list-pokemon">
+          {Array.isArray(pokemons) && pokemons.map((pokemon, index) => (
+            <div key={index}>
+              <PokemonCard pokemon={pokemon} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
