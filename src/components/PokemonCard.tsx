1b480a3e1e40bec4325550d72d66b33fe67e3a9f
rigolinux <rigoberto7567@gmail.com>
2024-06-05 14:03:37 -0600

    create a pokemon card

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,31 @@
+
+import { Link } from 'react-router-dom';
+import { Pokemons } from '../types/pokemon';
+
+const PokemonCard = ({ pokemon }: { pokemon: Pokemons }) => {
+	return (
+		<Link to={`/pokemon/${pokemon.id}`} className='card-pokemon'>
+            <div className='card-img'>
+                {pokemon.sprites.other && (
+                    <img
+                        src={pokemon.sprites.other.dream_world.front_default}
+                        alt={`Pokemon ${pokemon.name}`}
+                    />
+                )}
+            </div>
+			<div className='card-info'>
+				<span className='pokemon-id'>N° {pokemon.id}</span>
+				<h3>{pokemon.name}</h3>
+				<div className='card-types'>
+					{pokemon.types.map(type => (
+						<span key={type.type.name} className={type.type.name}>
+							{type.type.name}
+						</span>
+					))}
+				</div>
+			</div>
+		</Link>
+	);
+};
+
+export default PokemonCard;
