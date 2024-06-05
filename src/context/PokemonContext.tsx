8e9bab59cfa3404915d7e1b2f81a265d29b02796
rigolinux <rigoberto7567@gmail.com>
2024-06-05 14:03:05 -0600

    update pokemon context

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -1,5 +1,5 @@
-import  { createContext, useState, useEffect, ReactNode } from 'react';
-import { getAllPokemons } from '../api/pokemons';
+import { createContext, useState, useEffect, ReactNode, useCallback } from 'react';
+import { getAllPokemons, getAllPokemonsLight, getPokemon } from '../api/pokemons';
 import { Pokemons } from '../types/pokemon';
 
 interface PokemonContextProps {
@@ -9,15 +9,38 @@ interface PokemonContextProps {
   nextPage: () => void;
   prevPage: () => void;
   setPage: (page: number) => void;
+  filterPokemons: (search: string) => void;
+  search: string;
+  setSearch: (search: string) => void;
 }
 
 export const PokemonContext = createContext<PokemonContextProps | undefined>(undefined);
 
 export const PokemonProvider = ({ children }: { children: ReactNode }) => {
   const [pokemons, setPokemons] = useState<Pokemons | [] | unknown>([]);
+  const [pokemonsComplete, setPokemonsComplete] = useState<Pokemons | [] | unknown>([]);
   const [offset, setOffset] = useState(0);
   const [maxOffset, setMaxOffset] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemons | [] | unknown>([]);
+
+  const customOffset = useCallback(async () => {
+    try {
+      console.log("customOffset", filteredPokemons);
+      const start = (currentPage - 1) * 25;
+      const end = currentPage * 25;
+      const promises: Promise<Pokemons>[] = (filteredPokemons as Pokemons[]).slice(start, end).map((pokemon: Pokemons) => {
+        return getPokemon(pokemon.name);
+      });
+      console.log("promises", promises);
+      const data = await Promise.all(promises);
+      setPokemons(data);
+    } catch (error) {
+      console.error('Error fetching Pokémon:', error);
+    }
+  }, [currentPage, filteredPokemons]);
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -28,13 +51,21 @@ export const PokemonProvider = ({ children }: { children: ReactNode }) => {
           const maxOffset = Math.ceil(data.count / 25);
           setMaxOffset(maxOffset);
         }
+        const dataComplete = await getAllPokemonsLight();
+        if (dataComplete) {
+          setPokemonsComplete(dataComplete);
+        }
       } catch (error) {
         console.error('Error fetching Pokémon:', error);
       }
     };
 
-    fetchPokemons();
-  }, [offset]);
+    if (!isFiltered) {
+      fetchPokemons();
+    } else {
+      customOffset();
+    }
+  }, [offset, isFiltered, customOffset]);
 
   const nextPage = () => {
     setCurrentPage(prevPage => prevPage + 1);
@@ -51,8 +82,35 @@ export const PokemonProvider = ({ children }: { children: ReactNode }) => {
     setOffset((page - 1) * 25);
   };
 
+  const filterPokemons = async () => {
+    setIsFiltered(true);
+    setCurrentPage(1);
+    setOffset((currentPage - 1) * 25);
+
+    const filtered = (pokemonsComplete as Pokemons[]).filter((pokemon: Pokemons) => {
+      return pokemon.name.includes(search);
+    });
+    setFilteredPokemons(filtered);
+    const promises: Promise<Pokemons>[] = filtered.slice(0, 25).map((pokemon: Pokemons) => {
+      return getPokemon(pokemon.name);
+    });
+    console.log("promises", promises);
+    setMaxOffset(Math.ceil(filtered.length / 25));
+
+    const data = await Promise.all(promises);
+    setPokemons(data);
+  };
+
+  useEffect(() => {
+    if (!search) {
+      setIsFiltered(false);
+      setOffset(1);
+      setCurrentPage(1);
+    }
+  }, [search]);
+
   return (
-    <PokemonContext.Provider value={{ pokemons, currentPage, maxOffset, nextPage, prevPage, setPage }}>
+    <PokemonContext.Provider value={{ pokemons, currentPage, maxOffset, nextPage, prevPage, setPage, filterPokemons, search, setSearch }}>
       {children}
     </PokemonContext.Provider>
   );
